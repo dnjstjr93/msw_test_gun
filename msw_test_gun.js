54ee8b837cc4be06bf2b6dad5d14021321217194
IoTKETI <IoTKETI@github.com>
2021-01-12 13:31:22 +0900

    commit by KETI

--- msw_test_gun.js
+++ msw_test_gun.js
@@ -309,6 +309,9 @@ function msw_mqtt_connect(broker_ip, port) {
 //////////////////////////////////////////////////////////////////////////////
 function parseDataMission(topic, str_message) {
   try {
+        var obj_lib_data = JSON.parse(str_message);
+         if(fc.hasOwnProperty('global_position_int')) {             Object.assign(obj_lib_data, JSON.parse(JSON.stringify(fc['global_position_int'])));
+         }         str_message = JSON.stringify(obj_lib_data);
   }
   catch (e) {
     console.log('[parseDataMission] data format of lib is not json');
@@ -318,6 +321,9 @@ function parseDataMission(topic, str_message) {
 
 function parseControlMission(topic, str_message) {
   try {
+        var topic_arr = topic.split('/');
+         var _topic = '/MUV/control/' + config.lib[0].name + '/' + topic_arr[topic_arr.length - 1];
+         msw_mqtt_client.publish(_topic, str_message);
   }
   catch (e) {
     console.log('[parseDataMission] data format of lib is not json');
